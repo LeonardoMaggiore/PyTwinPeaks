759331e6f49cea120b064cdc11f29cdc322c15fa
Leonardo Maggiore <127802470+LeonardoMaggiore@users.noreply.github.com>
2025-11-11 16:37:33 +0100

    Create setup.py
    Creation of setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyTwinPeaks',
+    version='0.1.0',
+    description='Tunnel void finder in weak lensing maps',
+    author='Leonardo Maggiore',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'scikit-image',
+        'astropy',
+        'imageio',
+        'opencv-python',
+        'python-magic',
+        'camb',
+        'getdist',
+        'ipython',
+    ],
+    include_package_data=True,
+)
